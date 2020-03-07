441c4a49a02cdb3592921566cb221524561a143b
LeeAA20 <60363283+LeeAA20@users.noreply.github.com>
2020-03-07 15:50:48 -0500

    ingredients GUI

---
+++ GJ1UnityProject/Assets/Pumpkin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pumpkin : MonoBehaviour
+{
+    Image BWPumpkin;
+    public Sprite PumpkinColor;
+    public static int PumpkinAmount;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            PumpkinAmount += 1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        BWPumpkin = GameObject.Find("BWPumpkin").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PumpkinAmount == 1) {
+            BWPumpkin.sprite = PumpkinColor;
+        }
+    }
+}
